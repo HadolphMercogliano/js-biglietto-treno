94cea51f4ea52bf54944fa432578df3231c5867b
HadolphMercogliano <giovanni.mercogliano@gmail.com>
2023-01-23 16:53:04 +0100

    scomposizione problema

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+// Il prezzo del biglietto è definito in base ai km(0.21 € al km).
+// Va applicato uno sconto del 20 % per i minorenni.
+// Va applicato uno sconto del 40 % per gli over 65.
+// L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+
+// SVOLGIMENTO
+
+// 1.CHIEDI ALL' UTENTE QUANTIN CHILOMETRI VUOLE PERCORRERE
+
+// 2.CHIEDI L'ETà DEL PASSEGGERO
+
+// 3.SE IL PASSEGGERO HA MENO DI 18 ANNI IL BIGLIETTO è SCONTATO DEL 20%
+
+// 4.SE IL PASSEGGERO HA 65 ANNI O PIU' IL BIGLIETTO è SCONTATO DEL 40%
+
+// 5. SCRIVI L'OUTPUT DEL PREZZO CON 2 CIFRE DECIMALI
